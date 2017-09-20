f95fc01b03c55c5859c84e6ce419579a771513ab
杨常辉 <yangch@culiu.org>
2017-09-20 18:33:47 +0800

    param

--- src/main/java/com/glory/maven/GeneratorCode.java
+++ src/main/java/com/glory/maven/GeneratorCode.java
@@ -1,4 +1,4 @@
-/*
+package com.glory.maven;/*
  * Copyright (C) 2017 www.chuchujie.com All rights reserved.
  *
  * Created by yangchanghui@chuchujie.com on 2017/9/20.
@@ -7,6 +7,7 @@
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
 import java.io.File;
 import java.io.IOException;
@@ -16,11 +17,19 @@ import java.io.IOException;
  *
  * @author yangchanghui@chuchujie.com
  */
-@Mojo(name = "hello")
+@Mojo(name = "auto")
 public class GeneratorCode extends AbstractMojo {
+    @Parameter(property = "baseDir",defaultValue = "")
+    private String baseDir;
+
     public void execute() throws MojoExecutionException {
-        getLog().info("123");
-        File file = new File("hello.txt");
+        getLog().info("invoke auto plugin..." + baseDir);
+        StringBuilder path = new StringBuilder();
+        if (baseDir != null && !baseDir.equals("")) {
+            path.append(baseDir);
+        }
+        path.append("hello.txt");
+        File file = new File(path.toString());
         if (!file.exists()) {
             try {
                 if (file.createNewFile()) {
